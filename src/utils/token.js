804005b70328e1ba12a1a979c128f867701662d8
ichthus <jofrodriguezsu@gmail.com>
2025-12-27 15:01:30 -0500

    modification expiration time in tokens

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,11 +1,11 @@
 const jwt = require("jsonwebtoken")
 
 const accessToken = (payload)=>{
-  return jwt.sign(payload, process.env.SECRET_ACCESS_TOKEN, {expiresIn:"1m"})
+  return jwt.sign(payload, process.env.SECRET_ACCESS_TOKEN, {expiresIn:"1h"})
 }
 
 const refreshToken = (payload)=>{
-  return jwt.sign(payload, process.env.REFRESH_TOKEN, {expiresIn: "5m"})
+  return jwt.sign(payload, process.env.REFRESH_TOKEN, {expiresIn: "7d"})
 }
 
 module.exports = {accessToken, refreshToken}
